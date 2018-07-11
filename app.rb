3fc6444f2e5bb729c8c603a4baab6f565485ce32
Vishal Sharma <vsh1819@gmail.com>
2018-07-11 14:52:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -38,7 +38,7 @@ class App < Sinatra::Base
     @operation = params[:operation]
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
-    @calculation = 
+    @calculation = nil
     if "sum" == @operation
       return @calculation = @number1 + @number2
       elsif "subtract" == @operation
