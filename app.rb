1c0290a38387e56230bb22f129ad9ead4cfdb4e8
Vishal Sharma <vsh1819@gmail.com>
2018-07-11 14:49:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -35,7 +35,7 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
+    @operation = params[:operation].to_sym
     @number1 = params[:number1].to_i
     @number2 = params[:number2].to_i
     if "sum" == @operation
