cb6a5668f615b5f8ccb11bed52d37aed83dfca95
Vishal Sharma <vsh1819@gmail.com>
2018-07-11 14:35:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class App < Sinatra::Base
     @phrase = params[:phrase]
     @num = params[:number].to_i
     @new_phrase = ""
-    @phrase.split("")
+    @phrase.split("%20")
     @result = ""
     @num.times do
       @result += @phrase
